f49f42eae0337b6bfb568860d49bd12485f04f36
taylor.holmes <taylorh2343@gmail.com>
2021-12-16 00:51:23 -0600

    user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,14 @@ var getUserRepos = function() {
     console.log("function was called");
   };
   
-  var getUserRepos = function(){
-      fetch("https://api.github.com/users/octocat/repos");
+  var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
   };
