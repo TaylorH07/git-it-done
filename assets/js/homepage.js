b02506b3ed77b924bb49b38f1a48a85843f7a5ec
taylor.holmes <taylorh2343@gmail.com>
2021-12-27 19:21:43 -0600

    api function buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,24 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var buttonClickHandler = function(event){
+
+  var language = event.target.getAttribute("data-language");
+  console.log(language);
+
+  if (language) {
+    getFeaturedRepos(language);
+  
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+};
+
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
+
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -88,6 +106,20 @@ var displayRepos = function(repos, searchTerm) {
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
   }
+};
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
 };
 
 // add event listeners to forms
